eace02dad5d2029bcb134ca3ea33d211e583f27f
Abuzar-Sheikh-20 <ziddishahzada2006@gmail.com>
2025-01-24 11:31:06 +0530

    adding auth.js file which left before

--- Back-end/middleware/auth.js
+++ Back-end/middleware/auth.js
@@ -20,26 +20,23 @@
 
 // export default authUser
 
+
+
 import jwt from "jsonwebtoken";
 
 const authUser = async (req, res, next) => {
-  // Correctly access the token from the headers (Authorization header is typical)
   const authHeader = req.headers.authorization;
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.json({ success: false, message: "Not authorized, Login again" });
   }
 
-  const token = authHeader.split(" ")[1]; // Extract the actual token from the "Bearer tokenString"
+  const token = authHeader.split(" ")[1];
 
   try {
-    // Verify the token
     const token_decode = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
-    // Attach userId to the request body
     req.body.userId = token_decode.id;
-
-    // Proceed to the next middleware
     next();
   } catch (error) {
     console.log(error);
